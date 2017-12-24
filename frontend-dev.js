048199ddf095c68953685860c41d954471ad9750
evandalong <evandalong@gmail.com>
2017-12-24 12:02:53 -0500

    Fix nav element highlighting not working properly

--- frontend-dev.js
+++ frontend-dev.js
@@ -6,7 +6,8 @@ const app = middleware(new FloofBall());
 app.get('/dev').exec((req, ren) => ren.render('dev/home.html'));
 
 app.get('/dev/dash').exec((req, ren) => {
-  
+  if (!req.user) return redirect('/login');
+  return ren.render('dev/dash.html');
 });
 
 app.get('/dev/dash/:cid').withQuery('p', 'str').exec((req, ren) => {
